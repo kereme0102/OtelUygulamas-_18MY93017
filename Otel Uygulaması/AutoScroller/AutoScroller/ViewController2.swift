59fba505c29df6a25c38371a8699b7485b4d79f1
kereme0102 <61414445+kereme0102@users.noreply.github.com>
2020-05-01 13:46:13 +0300

    Otel Uygulaması
    Kerim Eraslan Halil İbrahim Zırh

---
+++ Otel Uygulaması/AutoScroller/AutoScroller/ViewController2.swift
@@ -0,0 +1,12 @@
+//
+//  ViewController2.swift
+//  AutoScroller
+//
+//  Created by YilmazC on 26.03.2020.
+//  Copyright © 2020 SHUBHAM AGARWAL. All rights reserved.
+//
+
+import Foundation
+
+
+
